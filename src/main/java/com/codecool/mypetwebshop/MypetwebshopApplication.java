be196f4e6e88b7acee1bbbdfbb6daa472fd10012
vadnaireka <budaikreativ@gmail.com>
2019-10-07 14:43:20 +0200

    first commit

---
+++ src/main/java/com/codecool/mypetwebshop/MypetwebshopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mypetwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetwebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetwebshopApplication.class, args);
+    }
+
+}
